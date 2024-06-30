d8b9a202ffa91f6c6e17fd4ea354ea75b6912974
infosalim <salimh5757@gmail.com>
2024-06-30 20:19:35 +0600

    feat:next auth items

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,42 @@
-import { bids as bidsSchema } from "@/db/schema";
+import { bids as bidsSchema, items } from "@/db/schema";
 import { database } from "@/db/database";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/app/auth";
 
 export default async function Home() {
-  const bids = await database.query.bids.findMany();
+
+  const session = await auth();
+
+  const allItems = await database.query.items.findMany();
+
+  if(!session) return null;
+  const user = session.user;
+  if(!user) return null;
 
   return (
     <main className="container mx-auto py-12">
+      {session ? <SignOut /> : <SignIn />}
+      {session?.user && session?.user.name}
       <form action={async (formData: FormData) => {
         "use server";
         // const bid = formData.get("bid") as string;
-        await database?.insert(bidsSchema).values({});
+        await database?.insert(items).values({
+          name: formData.get("name") as string,
+          userId: session?.user?.id!,
+        });
         revalidatePath('/');
       }}>
-        <Input name="bid" placeholder="Bid" />
-        <Button type="submit">Place Bid</Button>
+        <Input name="name" placeholder="Name your item" />
+        <Button type="submit">Post Item</Button>
 
       </form>
-      {bids.map((bid) => (
-        <div key={bid.id}>{bid.id}</div>
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
       ))}
     </main>
   );
